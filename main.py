28b8ba715ef5625de0875d1c6045008227eb0cae
Rooginho <116230386+Rooginho@users.noreply.github.com>
2022-10-20 01:19:24 +0000

    aprendendo minha idade e meu nome

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ idade = 23
 # Exibir o nome na tela (que esta dentro da variavel nome)
 print(nome)
 
+#Quando quiser exibir a frase "minha idade é" completando com a variavel da idade
+#Print("minha idade é"+idade)
+print("Minha idade é " +str (idade)+"anos\n")
+print(f"Minha idade é {idade}+anos\n")
+print("Minha idade é {}".format(idade)+"anos\n")
+
+#Quando quiser exibir "meu nome é...e tenho...anos " trocando pelas variaveis nome e idade
+print("Meu nome é {} e tenho {} anos".format(nome, idade))
